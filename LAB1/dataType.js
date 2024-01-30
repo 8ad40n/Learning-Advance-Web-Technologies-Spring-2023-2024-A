75ef0dc05f8c0d9f6b27c990e78eb9179a31d8a9
Badhon Nath Joy <joy184110@gmail.com>
2024-01-30 13:03:08 +0600

    Basic js added

---
+++ LAB1/dataType.js
@@ -0,0 +1,17 @@
+// string
+let firstName= "john";
+console.log(firstName);
+
+// number
+let cgpa= 3.75;
+console.log(cgpa);
+
+// Boolean
+let isOkay= true;
+console.log(isOkay);
+
+let result= 0/0;
+console.log(result);
+
+let notANumber= parseInt("Hello");
+console.log(notANumber);
