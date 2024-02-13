a20e8331a929c76e2b753a765c0bf6d657ea3410
Badhon Nath Joy <joy184110@gmail.com>
2024-02-13 20:54:01 +0600

    Added Node and nest

--- Js basic/conditional_operator.js
+++
@@ -1,3 +0,0 @@
-let age = 20;
-let status = (age>=18) ? "Adult" : "Minor";
-console.log(status);
--- Js basic/variable.js
+++
@@ -1,8 +0,0 @@
-var name= "Badhon Nath Joy";
-let age= 24;
-const pi= 3.14;
-
-console.log(name);
-console.log(age);
-console.log(pi);
-
