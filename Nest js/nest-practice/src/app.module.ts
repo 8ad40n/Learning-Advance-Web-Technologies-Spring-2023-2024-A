76fb14dfc52ec55962cb5f6c01aee0f9471f17d0
Badhon Nath Joy <joy184110@gmail.com>
2024-02-15 14:41:37 +0600

    Module added

--- Nest js/nest-practice/src/app.module.ts
+++ Nest js/nest-practice/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, OrdersModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: []
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+    console.log("App Module");
+  }
+}
