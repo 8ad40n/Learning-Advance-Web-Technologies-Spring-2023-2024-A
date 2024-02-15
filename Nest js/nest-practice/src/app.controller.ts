8e0d2f33a9c0464031868ece694a4eb04751ea5b
Badhon Nath Joy <joy184110@gmail.com>
2024-02-15 16:08:58 +0600

    Controllers added

--- Nest js/nest-practice/src/app.controller.ts
+++ Nest js/nest-practice/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,32 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // add book
+  @Post('/add')
+  addBook(): string {
+    return 'This will add book';
+  }
+  // delete book
+  @Delete('/delete')
+  deleteBook(): string {
+    return 'This will delete book';
+  }
+  // update book
+  @Put('/update')
+  updateBook(): string {
+    return 'This will update book';
+  }
+  // find all books
+  @Get('/findAll')
+  findAllBook(): string {
+    return 'This will find all books';
+  }
+
+  @Get('/findBookById/:id')
+  findBookById(@Param() params: any): string {
+    console.log(params.id);
+    return `This will find a book of ID: #${params.id}`;
   }
 }
