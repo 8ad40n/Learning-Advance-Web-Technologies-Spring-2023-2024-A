eb8615dbfd5ae442142b1add464674c05e3a29ed
8ad40n <joy184110@gmail.com>
2024-04-16 13:50:40 +0600

    Final Lab Task 1

--- React/landingPage/src/components/skill.tsx
+++ React/landingPage/src/components/skill.tsx
@@ -1,7 +1,7 @@
 function Skill() {
     // const [count, setCount] = useState(0)
   
-    const skill= ["Typescript, ", "Nest.js, ", "React.js, ", "Next.js, " , "Javascript, ", "Postgresql, ", "REST api "]
+    const skill= ["Typescript, ", "Nest.js, ", "React.js, ", "Next.js, " , "Javascript, ", "Postgresql, ", "REST api, " , "Ms SQL Server, " , ".NET, ", "C++, " , "Python "]
     return (
       <>
       <div className="component">
