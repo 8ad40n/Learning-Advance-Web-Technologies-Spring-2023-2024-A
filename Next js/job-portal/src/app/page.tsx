eeda706b46ad853a2c69ef36328e31c963c28c93
8ad40n <joy184110@gmail.com>
2024-04-23 13:03:49 +0600

    Job portal final task 2

---
+++ Next js/job-portal/src/app/page.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Employee Management System</h1><br /><br />
+      <p>This system allows you to manage employee information.</p><br /><br />
+      <ul>
+        <li>
+          <Link href="/register">Register Employee</Link>
+        </li>
+        <li>
+          <Link href="/update">Update Employee</Link>
+        </li>
+        <li>
+          <Link href="/delete">Delete Employee</Link>
+        </li>
+        <li>
+          <Link href="/search">Search Employee</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
